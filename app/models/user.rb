1686f84cc418a808fcfa7f1382ffc4731f970b25
ndinhbang <ndinhbang111@gmail.com>
2019-09-17 17:59:15 +0700

    kiểm tra định dạng email
    Sử dụng regular expression

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   # trường name không được bỏ trống, tối đa 50 kí tự
   validates :name,  presence: true, length: { maximum: 50 }
   # trường email không được bỏ trống, tối đa 255 kí tự
-  validates :email, presence: true, length: { maximum: 255 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
 end
