4e3fab6eb23794c41d9bdf5d963ddc6469d3b0a1
ndinhbang <ndinhbang111@gmail.com>
2019-09-17 17:55:41 +0700

    kiểm tra dộ dài name và email của user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  # trường name không được bỏ trống
-  validates :name, presence: true
-  # trường email không được bỏ trống
-  validates :email, presence: true
+  # trường name không được bỏ trống, tối đa 50 kí tự
+  validates :name,  presence: true, length: { maximum: 50 }
+  # trường email không được bỏ trống, tối đa 255 kí tự
+  validates :email, presence: true, length: { maximum: 255 }
 end
